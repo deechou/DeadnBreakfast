589a7d8993681d2a9bef0dce3cc01ca0a514200f
deechou <dchou1@gmail.com>
2023-12-07 06:44:43 -0500

    Added hurt animation, knockback when taking damage. Changed camera to smoothed
    Signed-off-by: deechou <dchou1@gmail.com>

--- Entities/player.gd
+++ Entities/player.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 @export var speed = 35
+@export var knockbackPower = 500
 @onready var animations = $AnimationPlayer
+@onready var effects = $Effects
+@onready var effectsTimer = $EffectsTimer
+
+
 
 signal healthChanged
+
+func _ready():
+	effects.play("RESET")
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -28,7 +36,21 @@ func _on_hurt_box_area_entered(area):
 	if area.name == "Hitbox":
 		Globals.playerCurrHealth -= 1
 		healthChanged.emit()
+		knockback(area.get_parent().velocity)
+		blinkRed()
 		if Globals.playerCurrHealth <= 0:
 			print_debug("YOU DIED")
+			Globals.playerCurrHealth = Globals.playerMaxHealth
 		else:
 			print_debug(Globals.playerCurrHealth)
+
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = (enemyVelocity-velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
+	move_and_slide()
+
+func blinkRed():
+	effects.play("blinkRed")
+	effectsTimer.start(0.75)
+	await effectsTimer.timeout
+	effects.play("RESET")
